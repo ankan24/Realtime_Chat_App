07257c69ef1f9e533597fd32b871e94a2116581b
Ankan Ghorai <ankanghorai30@gmail.com>
2024-12-30 00:32:18 +0530

    version 1 (26:00)

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = (req,res)=>{
+    res.send("Signup routes");
+}
+
+export const login = (req,res)=>{
+    res.send("Login routes");
+}
+
+
+export const logout = (req,res)=>{
+    res.send("Logout routes");
+}
